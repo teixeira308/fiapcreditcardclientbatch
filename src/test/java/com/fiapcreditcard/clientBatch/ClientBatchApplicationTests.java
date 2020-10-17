831f850dad055a2687c4a3d536ca94fdba2f1d88
teixeira308 <teixeira308@Hotmail.com>
2020-10-17 16:56:43 -0300

    test

--- src/test/java/com/fiapcreditcard/clientBatch/ClientBatchApplicationTests.java
+++ src/test/java/com/fiapcreditcard/clientBatch/ClientBatchApplicationTests.java
@@ -1,13 +1,66 @@
 package com.fiapcreditcard.clientBatch;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
+import static org.awaitility.Awaitility.await;
+
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {ClientBatchApplication.class, BatchConfig.class})
 class ClientBatchApplicationTests {
 
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private Job job;
+
+	@Autowired
+	private JobLauncherTestUtils jobLauncherTestUtils;
+
 	@Test
-	void contextLoads() {
+	public void batchProcessAlunoFileToDatabase() throws 	JobParametersInvalidException,
+			JobExecutionAlreadyRunningException,
+			JobRestartException,
+			JobInstanceAlreadyCompleteException,
+			SQLException
+	{
+		JobExecution jobExecution = jobLauncherTestUtils.getJobLauncher()
+				.run(job, jobLauncherTestUtils.getUniqueJobParameters());
+		Assertions.assertNotNull(jobExecution);
+		Assertions.assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
+
+		ResultSet resultSet = dataSource.getConnection()
+				.prepareStatement("select count(*) from TB_ALUNOS")
+				.executeQuery();
+
+		await().atMost(10, TimeUnit.SECONDS)
+				.until(() -> {
+							resultSet.last();
+							return resultSet.getInt(1) == 3;
+						}
+				);
+		Assertions.assertEquals(3, resultSet.getInt(1));
 	}
 
 }
